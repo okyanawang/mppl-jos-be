f51c1f5ceabc99e88c617b0f8248aa161cf0c33b
hufahamdika <hufahamdika7997@gmail.com>
2022-12-05 21:50:50 +0700

    add cors

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use app\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +22,13 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        
+        User::factory()->create([
+            'username' => 'benedictusbimo',
+            'first_name' => 'benedictus',
+            'last_name' => 'bimo',
+            'email' => 'benedictusbimo@gmail.com',
+            'password' => Hash::make('titusganteng')
+        ]);
     }
 }
