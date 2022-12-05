47d1391555a38b28eb493cfe2cd6c65453382cbe
hufahamdika <hufahamdika7997@gmail.com>
2022-12-05 21:41:54 +0700

    repair register

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -14,7 +14,9 @@ class AuthController extends Controller
     public function register(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'name' => 'required|string|max:255',
+            'username' => 'required|string|max:255',
+            'first_name' => 'required|string|max:255',
+            'last_name'=> 'required|string|max:255',
             'email' => 'required|string|max:255|unique:users',
             'password' => 'required|string|min:8'
         ]);
@@ -24,8 +26,10 @@ class AuthController extends Controller
         }
 
         $user = User::create([
-            'name' => $request->name,
+            'username' => $request->username,
             'email' => $request->email,
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
             'password' => Hash::make($request->password)
         ]);
 
